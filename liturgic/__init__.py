a0287af83fef7e465961c2f5f431dc2dc4b34a12
rousseldenis <roussel2nis@hotmail.com>
2016-10-02 19:06:33 +0200

    [IMP] Change implementation - use categories instead of new model - more convenient

--- liturgic/__init__.py
+++ liturgic/__init__.py
@@ -1,1 +1,1 @@
-import celebration
+from . import models
--- liturgic/__openerp__.py
+++ liturgic/__openerp__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name' : 'Liturgic',
-    'version' : '1.0',
+    'name': 'Liturgic',
+    'version': '9.0.1.0.0',
     'summary': 'Manage liturgic organizations',
     'sequence': 30,
     'description': """
@@ -10,15 +10,15 @@ Liturgic
 ========
 
     """,
-    'category' : 'Liturgic',
-    'images' : [],
-    'depends' : ['calendar','web_calendar'],
+    'category': 'Liturgic',
+    'images': [],
+    'depends': ['calendar', 'web_calendar'],
     'data': ['security/liturgic_security.xml',
-        'views/celebration_view.xml',
-        'security/ir.model.access.csv'
-        
-        
-    ],
+             'data/celebration_data.xml',
+             'views/celebration_view.xml',
+             'security/ir.model.access.csv',
+
+             ],
     'installable': True,
     'application': True,
     'auto_install': False,
